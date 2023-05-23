dfc8aede7990de92d4b114db47d83b2459d2f03e
zZeTraL <lucashbs.pro@gmail.com>
2023-05-23 20:36:23 +0200

    🌠 Update

---
+++ BonneEcole/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "twin.macro";
+
+const Home = () => {
+    return (
+        <p tw="flex">Hello World</p>
+    )
+}
+
+export default Home;
--- BonneEcole/vite.config.js
+++ BonneEcole/vite.config.js
@@ -3,5 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-macros',
+          [
+            '@emotion/babel-plugin-jsx-pragmatic',
+            {
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react',
+            },
+          ],
+          [
+            '@babel/plugin-transform-react-jsx',
+            { pragma: '__cssprop' },
+            'twin.macro',
+          ],
+        ],
+      },
+    }),
+  ],
 })
