83bfd1fc74e7af20a779709a7d2668bee6db0d1f
MaximeBonnel <maxime.bonnel@student.junia.com>
2023-05-29 20:21:48 +0200

    answers list

--- BonneEcole/src/CreateExercise/CreateExercise.jsx
+++ BonneEcole/src/CreateExercise/CreateExercise.jsx
@@ -3,15 +3,52 @@ import React, { useEffect, useState } from "react";
 // Scss
 import "./CreateExercise.scss"
 
+// Main page display
 const CreateExercise = (props) => {
-    const [previewSource, setPreviewSource] = useState("");
-    const [answers, setAnswers] = useState([]);
+    const [previewSource, setPreviewSource] = useState(""); // Preview image when uploded
+    let answers = []; // Answers list
+    const [inputAnswer, setInputAnswer] = useState(""); // Input answer
+
+
+    // Display the answers list
+    const displayList = () => {
+        if (answers.length > 0) {
+            setInputAnswer(
+                <ul>
+                  {answers.map((answer, index) => (
+                    <li key={index}>{answer}</li>
+                  ))}
+                </ul>
+            );
+        } else {
+            // If the list is empty, display a message
+            setInputAnswer(<p>Aucune réponse n'a été ajoutée</p>);
+        }
+    };
 
     useEffect(() => {
-        const uploadInput = document.getElementById('upload');
+        const uploadInput = document.getElementById('upload'); // Upload button
+        const answerInput = document.getElementById('answer'); // Answer input
+        const addAswerInput = document.getElementById('addAnswer'); // Add answer button
+        const addExerciseInput = document.getElementById('addExercise'); // Add exercise button
 
+        // Change the preview image when a new image is uploaded
         uploadInput.addEventListener('change', (event) => {
             setPreviewSource(URL.createObjectURL(event.target.files[0]));
+        });
+
+        // Add a new answer to the list
+        addAswerInput.addEventListener('click', (event) => {
+            answers.push(answerInput.value);
+            // annuler l'ajout si la réponse est vide
+            // annuler l'ajout si la réponse est déjà dans la liste
+            displayList();
+        });
+
+        // Delete an answer from the list
+
+        // Add the exercise to the database
+        addExerciseInput.addEventListener('click', () => {
         });
     }, []);
 
@@ -31,15 +68,17 @@ const CreateExercise = (props) => {
                 </div>
             </section>
 
+            <hr/>
+
             <section>
                 <h3>Réponses possibles</h3>
-                <input type="text"></input>
-                <button>Ajouter</button>
+                <input type="text" id="answer"></input>
+                <button id="addAnswer">Ajouter</button>
             </section>
 
-            <section><li>{answers}</li></section>
+            <section>{inputAnswer}</section>
 
-            <section><button>Créer l'exercice</button></section>
+            <section><button id="addExercise">Créer l'exercice</button></section>
         </>
     )
 };
