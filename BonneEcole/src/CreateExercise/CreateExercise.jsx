f8e71138f9ef869e85b996e1b9f18fa7fd6af68a
MaximeBonnel <maxime.bonnel@student.junia.com>
2023-05-30 12:18:34 +0200

    Statement exercise & QCM exercise

--- BonneEcole/src/CreateExercise/CreateExercise.jsx
+++ BonneEcole/src/CreateExercise/CreateExercise.jsx
@@ -3,91 +3,37 @@ import React, { useEffect, useState } from "react";
 // Scss
 import "./CreateExercise.scss"
 
+// Windows
+import StatementExercise from "./StatementExercise.jsx";
+/*import "./QCMExercise.jsx"*/
+
 // Main page display
 const CreateExercise = (props) => {
-    const [previewSource, setPreviewSource] = useState(""); // Preview image when uploded
-    let answers = []; // Answers list
-    const [inputAnswer, setInputAnswer] = useState(""); // Input answer
+    const [showStatementExercise, setShowStatementExercise] = useState(false);
+    const [showQCMExercise, setShowQCMExercise] = useState(false);
 
-
-    // Display the answers list
-    const displayList = () => {
-        if (answers.length > 0) {
-            setInputAnswer(
-                <ul>
-                  {answers.map((answer, index) => (
-                    <li key={index}>{answer}
-                    <img alt="delete" src="https://img.icons8.com/material-rounded/24/filled-trash.png" className="delete" onClick={() => {
-                        answers.splice(index, 1);
-                        displayList();
-                    }}>
-                    </img></li>
-                  ))}
-                </ul>
-            );
-        } else {
-            // If the list is empty, display a message
-            setInputAnswer(<p>Aucune réponse n'a été ajoutée</p>);
-        }
+    const openWindow1 = () => {
+        setShowStatementExercise(true);
+        setShowQCMExercise(false);
     };
 
-    useEffect(() => {
-        const uploadInput = document.getElementById('upload'); // Upload button
-        const answerInput = document.getElementById('answer'); // Answer input
-        const addAswerInput = document.getElementById('addAnswer'); // Add answer button
-        const addExerciseInput = document.getElementById('addExercise'); // Add exercise button
-
-        // Change the preview image when a new image is uploaded
-        uploadInput.addEventListener('change', (event) => {
-            setPreviewSource(URL.createObjectURL(event.target.files[0]));
-        });
-
-        // Add a new answer to the list
-        addAswerInput.addEventListener('click', (event) => {
-            // annuler l'ajout si la réponse est vide
-            // annuler l'ajout si la réponse est déjà dans la liste
-            if (answerInput.value != "" && !answers.includes(answerInput.value)) {
-                answers.push(answerInput.value);
-            }
-            displayList();
-        });
-
-        // Delete an answer from the list
-
-        // Add the exercise to the database
-        addExerciseInput.addEventListener('click', () => {
-        });
-    }, []);
+    const openWindow2 = () => {
+        setShowStatementExercise(false);
+        setShowQCMExercise(true);
+    };
 
     return (
         <>
             <h1>Créer votre propre exercice</h1>
 
-            <section>
-                <div>
-                    <h3>Énoncé</h3>
-                    <textarea name="statement" className="statement" required defaultValue="Énoncé de l'exercice..."></textarea>
-                </div>
-                <div className="image">
-                    <h3>Ajouter une image (optionnel)</h3>
-                    <input type="file" id="upload" accept="image/*"></input>
-                    <img className="preview" src={previewSource} alt="Image preview"></img>
-                </div>
-            </section>
+            <div className="switchWindow">
+                <button onClick={openWindow1}>Fenêtre 1</button>
+                <button onClick={openWindow2}>Fenêtre 2</button>
+            </div>
 
-            <hr/>
-
-            <section>
-                <h3>Réponses possibles</h3>
-                <input type="text" id="answer"></input>
-                <button id="addAnswer">Ajouter</button>
-            </section>
-
-            <section>{inputAnswer}</section>
-
-            <section><button id="addExercise">Créer l'exercice</button></section>
+            {showStatementExercise && <StatementExercise />}
         </>
-    )
+    );
 };
 
 export default CreateExercise;
---
+++ BonneEcole/src/CreateExercise/StatementExercise.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+
+const StatementExercise = (props) => {
+    const [previewSource, setPreviewSource] = useState(""); // Preview image when uploded
+    let answers = []; // Answers list
+    const [inputAnswer, setInputAnswer] = useState(""); // Input answer
+
+    // Display the answers list
+    const displayList = () => {
+        if (answers.length > 0) {
+            setInputAnswer(
+                <ul>
+                    {answers.map((answer, index) => (
+                    <li key={index}>{answer}
+                    <img alt="delete" src="https://img.icons8.com/material-rounded/24/filled-trash.png" className="delete" onClick={() => {
+                        // Delete an answer from the list
+                        answers.splice(index, 1);
+                        displayList();
+                    }}>
+                    </img></li>
+                    ))}
+                </ul>
+            );
+        } else {
+            // If the list is empty, display a message
+            setInputAnswer(<p>Aucune réponse n'a été ajoutée</p>);
+        }
+    };
+
+    useEffect(() => {
+        const uploadInput = document.getElementById('upload'); // Upload button
+        const answerInput = document.getElementById('answer'); // Answer input
+        const addAswerInput = document.getElementById('addAnswer'); // Add answer button
+        const addExerciseInput = document.getElementById('addExercise'); // Add exercise button
+
+        // Change the preview image when a new image is uploaded
+        uploadInput.addEventListener('change', (event) => {
+            setPreviewSource(URL.createObjectURL(event.target.files[0]));
+        });
+
+        // Add a new answer to the list
+        addAswerInput.addEventListener('click', (event) => {
+            // annuler l'ajout si la réponse est vide
+            // annuler l'ajout si la réponse est déjà dans la liste
+            if (answerInput.value != "" && !answers.includes(answerInput.value)) {
+                answers.push(answerInput.value);
+            }
+            displayList();
+        });
+
+        // Add the exercise to the database
+        addExerciseInput.addEventListener('click', () => {
+        });
+    }, []);
+
+    return (
+        <>
+            <section>
+                <div>
+                    <h3>Énoncé</h3>
+                    <textarea name="statement" className="statement" required defaultValue="Énoncé de l'exercice..."></textarea>
+                </div>
+                <div className="image">
+                    <h3>Ajouter une image (optionnel)</h3>
+                    <input type="file" id="upload" accept="image/*"></input>
+                    <img className="preview" src={previewSource} alt="Image preview"></img>
+                </div>
+            </section>
+
+            <hr/>
+
+            <section>
+                <h3>Réponses possibles</h3>
+                <input type="text" id="answer"></input>
+                <button id="addAnswer">Ajouter</button>
+            </section>
+
+            <section>{inputAnswer}</section>
+
+            <section><button id="addExercise">Créer l'exercice</button></section>
+        </>
+    );
+};
+
+export default StatementExercise;
