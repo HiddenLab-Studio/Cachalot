7649d9be4f4a0481cdf399a221d7ff0629c1bbee
zZeTraL <lucashbs.pro@gmail.com>
2023-05-30 03:39:03 +0200

    🌠 Update - Beginning of the home page main content from

--- Cachalot/src/pages/home/Home.jsx
+++ Cachalot/src/pages/home/Home.jsx
@@ -2,32 +2,25 @@ import tw, { styled } from "twin.macro";
 
 // Components
 import Navbar from "../../components/navbar/Navbar.jsx"
-import {AsideContainer, ContentContainer, MainContainer} from "./HomeStyle.js";
+import {AsideContainer, ContentContainer, MainContainer, MainSection} from "./HomeStyle.js";
 import TrendingExercise from "../../components/cards/TrendingExercise.jsx";
 
-const Container = styled.main`
-  ${tw`flex flex-row min-h-[100vh]`};
-  background-color: ${props => props.theme.background};
-  
-  @media (max-width: 768px) {
-    flex-direction: column-reverse;
-  }
-`;
+
 
 const Home = () => {
 
     return (
-        <Container>
+        <MainContainer>
             <Navbar/>
-            <MainContainer>
+            <MainSection>
                 <ContentContainer>
                     <TrendingExercise/>
                 </ContentContainer>
                 <AsideContainer>
                     <h1>aside content</h1>
                 </AsideContainer>
-            </MainContainer>
-        </Container>
+            </MainSection>
+        </MainContainer>
     )
 }
 
--- Cachalot/src/pages/home/HomeStyle.js
+++ Cachalot/src/pages/home/HomeStyle.js
@@ -10,10 +10,19 @@ export const ContentContainer = styled.div`
   flex-grow: 2;
 `
 
-export const MainContainer = styled.div`
+export const MainSection = styled.section`
   display: flex;
   flex-direction: row;
   flex-grow: 1;
   gap: 20px;
   padding: 25px;
 `
+
+export const MainContainer = styled.main`
+  ${tw`flex flex-row min-h-[100vh]`};
+  background-color: ${props => props.theme.background};
+  
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
+  }
+`;
