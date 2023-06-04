6f2e9efa34220d063e741f6bda8b722a2fbc39de
Antoine MIGNIEN <72608526+AntoineMIGNIEN@users.noreply.github.com>
2023-06-04 11:14:24 +0200

    update 🚀

--- server/src/firebase-client/firebase-accountSet.js
+++ server/src/firebase-client/firebase-accountSet.js
@@ -1,6 +1,6 @@
 // Import des fonctions dont on a besoin
 import { signOut, deleteUser } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
-import { collection, doc, deleteDoc, addDoc, getDoc, onSnapshot, query, orderBy,limit} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+import { collection, doc, deleteDoc, addDoc, getDoc, onSnapshot, query, orderBy, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 import { ref, onChildAdded } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
 import firebaseConfigClient from "../composable/firebaseConfigClient.js";
 
@@ -79,12 +79,13 @@ function sendMessage(e) {
         //On prend la date 
         const date = new Date();
         //On recupere l'heure et la date  
-        const hour =  date.toLocaleDateString() + " | " + date.toLocaleTimeString();
+        const hour = date.toLocaleDateString() + " | " + date.toLocaleTimeString();
         //On ajoute les info dans un objet
         const data = {
             message: message,
             user: username,
-            date: hour
+            date: hour,
+            like: 0,
         }
         //On ajoute le message dans la collection messages
         addDoc(collection(db, room()), data).then(() => {
@@ -102,16 +103,24 @@ window.message = function (e) {
 }
 
 
+//on recupere les messages envoyé 
 function getMessage() {
+    //On recupere la collection messages en fonction de le room
     const messagesCollection = collection(db, room());
+    //On recupere les messages par date croissante
     const message = query(messagesCollection, orderBy("date", 'asc'));
+
+    //On regarde le changement dans la collection
     onSnapshot(message, (snapshot) => {
         snapshot.docChanges().forEach((change) => {
+            //Si le changement est un ajout
             if (change.type === "added") {
+                //On recupere les données du message
                 const data = change.doc.data();
                 const message = data.message;
                 const user = data.user;
                 const date = data.date;
+                const like = data.like;
 
                 const messageList = document.getElementById('messageList');
                 const li = document.createElement('li');
@@ -120,11 +129,56 @@ function getMessage() {
                         <p class="message__user">${user}</p>
                         <p class="message__content">${message}</p>
                         <p class="message__date">${date}</p>
+                        <p class="message__like">${like}</p>
+                        <button id="like+${change.doc.id}" onclick="like(id)" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Like
+                        </button>
                     </div>`;
                 messageList.appendChild(li);
             }
         });
     });
+}
+
+/***** LIKE */
+window.like = function (id) {
+    //On recupere l'id du message avec le split qui prend le deuxieme element du "tableau"
+    const idMessage = id.split('+')[1];
+    const user = auth.currentUser;
+
+    console.log(idMessage);
+    console.log(room());
+    //On recupere le document du message
+    const docRef = doc(db, room(), idMessage);
+    //On recupere le document de l'utilisateur
+    const userDocRef = doc(db, room() + "/" + idMessage + "/like", user.uid);
+
+    //On regarde si l'utilisateur a deja like
+    if (userDocRef != null) {
+        console.log("Il a deja like");
+        return;
+    }
+    else {
+        //On uopdate le document
+        getDoc(docRef).then((doc) => {
+            //On recupere les likes
+            const like = doc.data().like;
+            //On update le document avec 1 like en plus 
+            updateDoc(docRef, {
+                like: like + 1,
+                //On update la collection des différene utilisateur qui ont like
+            }).then(() => {
+                console.log("Perfecto");
+                setDoc(userDocRef, {
+                    like: 1,
+                })
+            }).catch((error) => {
+                console.error("Error getting document:", error);
+            });
+        }).catch((error) => {
+            console.error("Error getting document:", error);
+        });
+    }
 }
 
 
