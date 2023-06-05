9658235a45dda221dfecc2a9205701ac5d083929
Leroy-Guillaume <70961560+Leroy-Guillaume@users.noreply.github.com>
2023-06-05 16:42:03 +0200

    systeme d'xp et de level up avec un boutton

--- server/src/firebase-client/firebase-accountSet.js
+++ server/src/firebase-client/firebase-accountSet.js
@@ -49,18 +49,87 @@ function clickDeleteAccount() {
         });
     })
 }
-
-function addXPToUser() {
+async function getXP() {
     const user = auth.currentUser;
-    const docRef = doc(db, 'users', user.uid)
-    getDoc(docRef).then((doc) => {
-        const xp = doc.data().xp;
-        updateDoc(docRef, {
-            xp: xp + 1,
-        })
-    })
-}             
+    const docRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(docRef);
+    const xp = docSnap.data().xp;
+    return xp;
+}
 
+async function getLevel() {
+    const user = auth.currentUser;
+    const docRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(docRef);
+    const level = docSnap.data().level;
+    return level;
+}
+
+async function addXPToUser() {
+    const user = auth.currentUser;
+    const docRef = doc(db, 'users', user.uid);
+
+    try {
+        const docSnap = await getDoc(docRef);
+        let xp = docSnap.data().xp;
+
+        if (typeof xp === 'number' && !isNaN(xp)) {
+            xp = Math.max(0, xp) + 1;
+            console.log(xp);
+        } else {
+            xp = 1;
+        }
+
+        await updateDoc(docRef, { xp: xp });
+        return xp; // Ajout de cette ligne pour renvoyer la nouvelle valeur de l'XP
+    } catch (error) {
+        console.error("Error updating XP:", error);
+        throw error; // Ajout de cette ligne pour propager l'erreur
+    }
+}
+
+
+async function levelUp() {
+    const user = auth.currentUser;
+    const docRef = doc(db, 'users', user.uid);
+
+    try {
+        const docSnap = await getDoc(docRef);
+        const xp = await addXPToUser(); // Utiliser la nouvelle valeur de xp renvoyée par addXPToUser()
+
+        let level = docSnap.data() && docSnap.data().level; // Vérifier si docSnap.data() existe et si level est défini
+        level = xp >= 10 ? Math.max(0, level || 0) + 1 : (level || 0);
+
+        console.log('Level up');
+        console.log(level);
+
+        await updateDoc(docRef, { xp: 0, level: level });
+    } catch (error) {
+        console.error("Error updating XP and level:", error);
+    }
+}
+
+// Récupérer la référence du bouton
+const xpButton = document.getElementById('xpButton');
+
+// Ajouter le gestionnaire d'événements au bouton
+xpButton.addEventListener('click', async () => {
+    console.log('click boutton xp');
+    const newXP = await addXPToUser();
+    if (newXP !== null && newXP >= 10) {
+        await levelUp();
+    }
+});
+
+
+
+async function getusername() {
+    const user = auth.currentUser;
+    const docRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(docRef);
+    const username = docSnap.data().username;
+    return username;
+}
 
 /**GET CANNAL */
 function room() {
@@ -71,49 +140,55 @@ function room() {
 window.room = async function (e) {
     const messageList = document.getElementById('messageList');
     messageList.innerHTML = "";
-    room()
+    await room();
     await getMessage();
 }
 
-/***** SEND MESSAGE */
-function sendMessage(e) {
+async function sendMessage(e) {
     e.preventDefault();
-    //On recupere l'utilisateur connecté
+    // On récupère l'utilisateur connecté
     const user = auth.currentUser;
-    //On recupere le username de l'utilisateur
-    let username = "Anonymous";
-    let xp = 0;
-    getDoc(doc(db, "users", user.uid)).then((doc) => {
-        username = doc.data().username;
-        //On recupere l'xp de l'utilisateur
-        xp = doc.data().xp;
-    }).then(() => {
-        //On recupere le message
-        const message = document.getElementById('inputMessage').value;
-        //On prend la date 
-        const date = new Date();
-        //On recupere l'heure et la date  
-        const hour = date.toLocaleDateString() + " | " + date.toLocaleTimeString();
+    // On récupère le username de l'utilisateur
+    let username = await getusername();
+    // On récupère l'xp de l'utilisateur
+    let xp = await getXP();
+    let level = await getLevel();
 
-        //On ajoute les info dans un objet
-        const data = {
-            message: message,
-            user: username,
-            date: hour,
-            like: 0,
-            xp: xp,
-        }
-        //On ajoute le message dans la collection messages
-        addDoc(collection(db, room()), data).then(() => {
-            document.getElementById('inputMessage').value = "";
-        })
-        addXPToUser();
+    if (!level) {
+        level = 0; // Valeur par défaut pour level
+    }
 
-    }).catch((error) => {
-        console.error("Error adding document: ", error);
-    });
 
+    //const docRef = doc(db, "users", user.uid);
+    //const docSnap = await getDoc(docRef);
+    //username = docSnap.data().username;
+
+    // On récupère le message
+    const message = document.getElementById('inputMessage').value;
+    // On prend la date 
+    const date = new Date();
+    // On récupère l'heure et la date  
+    const hour = date.toLocaleDateString() + " | " + date.toLocaleTimeString();
+
+    // On ajoute les infos dans un objet
+    const data = {
+        message: message,
+        user: username,
+        date: hour,
+        like: 0,
+        xp: xp,
+        level: level,
+    }
+
+    // On ajoute le message dans la collection messages
+    await addDoc(collection(db, room()), data);
+    document.getElementById('inputMessage').value = "";
+
+    await addXPToUser();
+    await levelUp();
 }
+
+
 
 window.message = function (e) {
     sendMessage(e);
