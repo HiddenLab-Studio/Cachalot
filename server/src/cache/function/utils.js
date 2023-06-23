f7d86899010230f7efc34ca2d457289163cd93d9
zZeTraL <lucashbs.pro@gmail.com>
2023-06-23 03:02:07 +0200

    🌠 Update [Exercise]
    - Everything is working
    - CumulatedDays from login on different date is working !

--- server/src/cache/function/utils.js
+++ server/src/cache/function/utils.js
@@ -3,7 +3,7 @@ const cacheManager = require("../cacheManager");
 
 const firebaseConfigClient = require("../../config/firebase.config");
 const { auth, db , storage } = firebaseConfigClient();
-const {doc, getDoc, updateDoc} = require("firebase/firestore");
+const {doc, getDoc, getDocs, updateDoc, collection} = require("firebase/firestore");
 
 const utils = {
     generateQuest: async(amount = 3) => {
@@ -57,6 +57,32 @@ const utils = {
 
         } else {
             console.log("[INFO] no user in the cache!");
+        }
+    },
+
+    updateQuestOfTheDay: async () => {
+        // return id of users in collection users
+        let users = [];
+        await getDocs(collection(db, "users")).then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                users.push({id: doc.id, userQuest: doc.data().userQuest});
+            });
+        });
+
+        // update quest of the day
+        for (let i = 0; i < users.length; i++) {
+            let id = users[i].id;
+            let userRef = doc(db, "users", id);
+            let data = users[i].userQuest;
+            data.currentQuest = await utils.generateQuest();
+            await updateDoc(userRef, {
+                userQuest: data
+            }).then(() => {
+                console.log("[INFO] user " + id + " data updated successfully!");
+                cacheManager.setUserToQuestCache(id, data);
+            }).catch((error) => {
+                console.log("[ERROR] Error updating user data:", error);
+            });
         }
     }
 }
