b5b51665299831fba6b99a1387bf35e2b4257a56
zZeTraL <lucashbs.pro@gmail.com>
2023-06-24 14:51:48 +0200

    🌠 Update [Css]
    - Css

--- Cachalot/src/pages/class/ClassHomePage.jsx
+++ Cachalot/src/pages/class/ClassHomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { styled } from "twin.macro";
 
 // Context
@@ -16,6 +16,8 @@ import BodyClass from './components/BodyClassComponents.jsx';
 
 // Styled Components
 import { MainContainer, Container } from "../../components/utils/ui/GlobalStyle.js";
+import SmallLoading from "../../components/utils/loading/SmallLoading.jsx";
+import Loading from "../../components/utils/loading/Loading.jsx";
 
 export const ClassContainer = styled(Container)``;
 export const Content = styled.section`
@@ -26,6 +28,18 @@ export const Content = styled.section`
   padding: 25px;
   gap: 32px;
   min-height: 100vh;
+  
+  .user__not__in__class {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: center;
+    h1 {
+      font-family: "Din_Round_Med", sans-serif;
+      color: ${props => props.theme.text};
+      font-size: var(--fs-sm);
+    }
+  }
   
   @media (min-width: 768px) and (max-width: 1050px) {
   max-width: 768px;
@@ -43,25 +57,60 @@ const ClassHomePage = () => {
     const cache = useCache();
 
     // State
-    const [isLoading, setIsLoading] = useState(!cache.isUserCached);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isInClass, setIsInClass] = useState(false);
+
+    useEffect(() => {
+        if(auth.currentUser instanceof Object) {
+            let classCode = window.location.pathname.split("/")[2];
+            setIsLoading(true);
+            auth.user.getUserClasses(auth.currentUser).then(r => {
+                r.forEach(c => {
+                    if(c.code === classCode) {
+                        setIsInClass(true);
+                    }
+                });
+                setIsLoading(false);
+            });
+        }
+    }, [auth.currentUser]);
 
     if(typeof auth.currentUser === "number") {
         return <ConnectionHomePage />
     } else {
         if (isLoading) {
-            return <FullLoading setIsLoading={setIsLoading} />
+            return <FullLoading setIsLoading={undefined} />
         } else {
-            return (
-                <MainContainer>
-                    <Navbar />
-                    <ClassContainer>
-                        <Content>
-                            <BodyClass auth={auth} />
-                            <AsideClass auth={auth}/>      
-                        </Content>
-                    </ClassContainer>
-                </MainContainer>
-            )
+            if(!isLoading) {
+                if(isInClass) {
+                    return (
+                        <MainContainer>
+                            <Navbar />
+                            <ClassContainer>
+                                <Content>
+                                    <BodyClass auth={auth} />
+                                    <AsideClass auth={auth}/>
+                                </Content>
+                            </ClassContainer>
+                        </MainContainer>
+                    )
+                } else {
+                    return (
+                        <MainContainer>
+                            <Navbar />
+                            <ClassContainer>
+                                <Content>
+                                    <div className="user__not__in__class">
+                                        <h1>Vous n'êtes pas dans cette classe</h1>
+                                    </div>
+                                </Content>
+                            </ClassContainer>
+                        </MainContainer>
+                    )
+                }
+            } else {
+                return <Loading />;
+            }
         }
     }
 }
